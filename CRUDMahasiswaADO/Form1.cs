585ca716d18f2651d13599eacc6c1799fc4498df
NatasyaAlia <natasyaalia436@gmail.com>
2026-04-09 18:08:19 +0700

    design form layout

--- CRUDMahasiswaADO/Form1.cs
+++ CRUDMahasiswaADO/Form1.cs
@@ -13,9 +13,13 @@ namespace CRUDMahasiswaADO
 {
     public partial class Form1 : Form
     {
+        private readonly SqlConnection conn;
+        private readonly string connectionString =
+            "Data Source=DESKTOP-8SMC21O\\NATASYA;Initial Catalog=DBAkademikADO;Integrated Security=True";
         public Form1()
         {
             InitializeComponent();
+            conn = new SqlConnection(connectionString);
         }
 
         private void Form1_Load(object sender, EventArgs e)
